0a0e1813623e28dc16f5056bb4f481171b5ec8cb
Rauane Lima <rauanelima7@gmail.com>
2023-01-02 17:01:16 -0300

    error: erros de deploy

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import "./app.css";
 import { Routes, Route } from "react-router-dom";
 import AllCountries from "./components/AllCountries/allcountries";
 import CountriesInfo from "./components/CountriesInfo/CountriesInfo";
-import { BiWorld } from "react-icons/bi";
 
 function App() {
   return (
     <>
-      <header className="header">
+      <div className="header">
         <div className="container">
-          <h4>
-            Conheça o Mundo!
-            <BiWorld />
-          </h4>
+          <h5>Where in the world</h5>
         </div>
-      </header>
+      </div>
       <div className="container">
         <Routes>
           <Route path="/" element={<AllCountries />} />
